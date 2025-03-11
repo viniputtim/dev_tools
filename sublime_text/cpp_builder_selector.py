4c851e80f3cffa10cf6bf589431e63ab2eeb99e6
viniputtim <viniputtim@gmail.com>
2025-03-10 22:49:41 -0300

    formated code

--- sublime_text/cpp_builder_selector.py
+++ sublime_text/cpp_builder_selector.py
@@ -8,4 +8,6 @@ packages_dir = '/home/viniciuspm/.config/sublime-text/Packages/User'
 if sys.argv[1].endswith('.h') or sys.argv[1].endswith('.hpp'):
     os.system(f'python3 "{packages_dir}/cpp_header_to_source.py" {sys.argv[1]}')
 else:
-    os.system(f'python3 "{packages_dir}/cpp_builder.py" {sys.argv[1]} {sys.argv[2]} {sys.argv[3]}')
+    os.system(
+        f'python3 "{packages_dir}/cpp_builder.py" {sys.argv[1]} {sys.argv[2]} {sys.argv[3]}'
+    )
--- sublime_text/cpp_header_to_source.py
+++ sublime_text/cpp_header_to_source.py
@@ -2,7 +2,6 @@ import os
 import sys
 
 
-# Inicialização de variáveis globais
 class_name = ''
 final_code = ''
 
@@ -24,17 +23,14 @@ def process_line(line):
 
     line = line.strip()
 
-    # Verifica se a linha contém a declaração de uma classe
     if 'class ' in line:
         class_name = line.replace('class', '').strip()
-    # Verifica se a linha termina com ';' (indicando uma declaração de método)
     elif line.endswith(');'):
         words = line.split()
         return_type = ''
         body = ''
         is_body = False
 
-        # Divide a linha em palavras e classifica como tipo e corpo
         for word in words:
             if '(' in word:
                 is_body = True
@@ -43,7 +39,6 @@ def process_line(line):
             else:
                 body += f'{word} '
 
-        # Adiciona o código da função ao final do código final
         final_code += f'\n\n\n{return_type}{class_name}::{body.replace(";", "").strip()}\n'
         final_code += '{\n}'
 
@@ -51,26 +46,19 @@ def process_line(line):
 def main():
     global final_code
 
-    # Pega o caminho do arquivo passado como argumento
     file_path = sys.argv[1]
     include_dir = 'include/'
 
-    # Encontra o caminho relativo
     rel_path = find_rel_path(file_path, include_dir)
-
-    # Inicializa a parte do include
     final_code += f'# include "{rel_path}"'
 
-    # Lê as linhas do arquivo
     code_lines = []
     with open(file_path) as file:
         code_lines = file.readlines()
 
-    # Processa cada linha do arquivo
     for line in code_lines:
         process_line(line)
 
-    # Exibe o código gerado
     print(final_code)
 
 
